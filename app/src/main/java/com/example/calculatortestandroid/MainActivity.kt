8cbb293b400e888bfabbf83e85f12d4cb79fa3ae
Ilya Burov <heaven30.buroff@gmail.com>
2020-09-20 08:52:16 +0300

    need to add scroll

--- app/src/main/java/com/example/calculatortestandroid/MainActivity.kt
+++ app/src/main/java/com/example/calculatortestandroid/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.calculatortestandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
 
-const val CALCULATE_EXPRESSION_FOR_SAVE = "clickCount";
-const val RESULT_FOR_SAVE = "result";
+
+const val TO_CALC_SAVE = "toCalc.text"
+const val TO_RES_SAVE = "result.text"
 
 class MainActivity : AppCompatActivity() {
 
@@ -65,21 +69,76 @@ class MainActivity : AppCompatActivity() {
         btnRb.setOnClickListener() {
             changeText(")")
         }
+        btnClear.setOnClickListener() {
+            toCalculate.text=""
+            result.text=""
+        }
+        btnDot.setOnClickListener() {
+            changeText(".")
+        }
+        btnBack.setOnClickListener(){
+            val test = toCalculate.text
+            if (test.isNotEmpty()){
+                toCalculate.text = test.substring(0, test.length-1)
+            }
+            result.text=""
+        }
+        result.setOnClickListener(View.OnClickListener {
+
+            var clipboardd = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            var clip = ClipData.newPlainText("label", result.text.toString())
+            clipboardd.setPrimaryClip(clip)
+        })
 
 
 
+
+        btnEvaluate.setOnClickListener(){
+            try{
+                val expression = ExpressionBuilder(toCalculate.text.toString()).build()
+                val total = expression.evaluate()
+
+                val forCheck1 = total.toLong()
+                val forCheck2 = total.toDouble()
+                if (total == forCheck1.toDouble()){
+                    result.text =forCheck1.toString()
+                }
+                else{
+                    result.text = total.toString()
+                }
+            }
+            catch (e:Exception) {
+                result.text="Invalid Expression"
+            }
+        }
     }
     override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(TO_CALC_SAVE, toCalculate.text.toString())
+        outState.putString(TO_RES_SAVE, result.text.toString())
         super.onSaveInstanceState(outState);
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        val toCalculateT = savedInstanceState.getString(TO_CALC_SAVE)
+        val resultT = savedInstanceState.getString(TO_RES_SAVE)
+        toCalculate.text = toCalculateT
+        result.text = resultT
         super.onRestoreInstanceState(savedInstanceState)
     }
 
     fun changeText (input:String) {
+        if (result.text=="Invalid Expression"){
+            result.text=""
+            toCalculate.text=""
+        }
+        if (result.text!=""){
+            toCalculate.text=result.text
+            result.text=""
+        }
         toCalculate.append(input.toString())
     }
+
+
 //    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
 //        super.onRestoreInstanceState(savedInstanceState)
 //
